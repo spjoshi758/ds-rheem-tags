d494b493a787f959114ca7bd5070c0803e6a5d33
Sagar Joshi <sagar.joshi@volansys.com>
2020-12-15 18:02:12 +0530

    version release

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ GIT_BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
 if [ $GIT_BRANCH = "master" ]
 then
     ver set --build ""
-    ver up
-    ver tag --patch
+    ver up --patch
+    ver tag
     git add .
     git commit -m "version release"
     git push origin --tags
