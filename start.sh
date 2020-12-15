96d594a377f8c640dc04449949023c79200fa72e
Sagar Joshi <sagar.joshi@volansys.com>
2020-12-15 18:00:17 +0530

    version release

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 GIT_BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
 
-echo "-----------Branch name is $GIT_BRANCH"
-
 if [ $GIT_BRANCH = "master" ]
 then
     ver set --build ""
