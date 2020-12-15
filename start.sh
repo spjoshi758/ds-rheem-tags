16024b40d423cc0a2cd8fafd5a34c6d828637f76
Sagar Joshi <sagar.joshi@volansys.com>
2020-12-15 18:01:04 +0530

    version release

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ if [ $GIT_BRANCH = "master" ]
 then
     ver set --build ""
     ver up
-    ver tag --minor
+    ver tag --patch
     git add .
     git commit -m "version release"
     git push origin --tags
